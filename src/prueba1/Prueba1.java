bf2ab6a895e2caf24da497663aca5a7f7d27c990
megas5 <megas5@megas5-pc>
2017-02-15 17:34:51 -0600

    se empezo el proyecto, esta sin acabar

---
+++ src/prueba1/Prueba1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba1;
+
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author megas5
+ */
+public class Prueba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame v1=new JFrame("Titulo de ventana");
+        JButton b1=new JButton("Cerrar");
+        
+        v1.setSize(300,200);
+        v1.setLayout(new FlowLayout());
+        
+        
+    }
+    
+}
